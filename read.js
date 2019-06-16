c659173422a72b4ea85d0ce473043618dc4063c3
simon3000 <simon3000@163.com>
2019-06-16 23:19:11 +0800

    lastHour and lastDay danmaku

--- read.js
+++ read.js
@@ -1,3 +1,39 @@
+const { InfluxDB } = require('influx')
+
 const Server = require('socket.io')
 const io = new Server(9009, { serveClient: false })
 
+require('dotenv').config()
+
+const config = require('./config')(process.env.DB_READUSER, process.env.DB_READPASS)
+const db = new InfluxDB(config)
+
+io.on('connection', socket => {
+  const handler = e => socket.on(e, async (data, arc) => {
+    if (typeof arc === 'function') {
+      if (e === 'lastHour') {
+        let query = await db.query('select content from danmaku where time > now() - 1h')
+        if (query.length) {
+          arc(query
+            .map(({ content }) => content)
+            .reduce((acc, cur) => acc + '\n' + cur))
+        } else {
+          arc('')
+        }
+      }
+      if (e === 'lastDay') {
+        let query = await db.query('select content from danmaku where time > now() - 1d')
+        if (query.length) {
+          arc(query
+            .map(({ content }) => content)
+            .reduce((acc, cur) => acc + '\n' + cur))
+        } else {
+          arc('')
+        }
+      }
+    }
+  })
+
+  handler('lastHour')
+  handler('lastDay')
+})
